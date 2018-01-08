8cef14144124b2fb6703f02533515292273a88fd
Vijay Dev <vijaydev.in@gmail.com>
2018-01-08 20:23:55 +0530

    connected scroll example 2 gen structure done

--- demo/src/connected-scroll/generateData.js
+++ demo/src/connected-scroll/generateData.js
@@ -11,12 +11,15 @@ const forEg1 = () => {
 }
 
 const forEg2 = () => {
+  const yCount = 75;
+  const xCount = 100;
+
   const yList = document.createElement('ul')
   const xHeading = document.createElement('div')
   const xGrid = document.createElement('div')
 
-  const yCount = 75;
-  const xCount = 100;
+  xHeading.classList.add('head-wrap')
+  xGrid.classList.add('grid-wrap')
 
   for (let i = 0; i < yCount; i++) {
     let child = document.createElement('li')
@@ -29,10 +32,14 @@ const forEg2 = () => {
     let gridRow = document.createElement('div')
     let child
 
+    headRow.classList.add('head-item')
+    gridRow.classList.add('grid-row')
+
     xHeading.appendChild(headRow)
     
     for (let j = 0; j < yCount; j++) {
       child = document.createElement('div')
+      child.classList.add('grid-item')
       gridRow.appendChild(child)
     }
 
