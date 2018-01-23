3a109007af97f441d220c30052a31002cf97acef
Vijay Dev <vijaydev.in@gmail.com>
2018-01-23 23:41:17 +0530

    lazy loading shell completed

---
+++ src/components/shared/Parent.js
@@ -0,0 +1,14 @@
+import { div } from 'styled-components'
+
+export const Parent = div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: 7rem;
+  background-color: #fff;
+  border-radius: 1rem;
+  overflow: hidden;
+  box-shadow: 0 .5rem 2rem rgba(0, 0, 0, 0.34);
+`
