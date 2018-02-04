225f25c407f4b9667fa0b5455c58eb13cb7a69b7
Vijay Dev <vijaydev.in@gmail.com>
2018-02-04 23:26:11 +0530

    completed exposed methods p1

--- demo/src/components/exposedMethods/Eg1.js
+++ demo/src/components/exposedMethods/Eg1.js
@@ -36,50 +36,49 @@ export default class ExposedMethodsEg1 extends React.Component {
   constructor(props) {
     super(props);
     this.$conatinerParent = document.querySelector('#container');
-    this.state = { actionData: [] }
-  }
-
-  componentWillMount() {
-    if (this.props.selectedMenu === 'load-exposed-methods-eg-1') {
-      this.setState({
-        actionData: [{
-          id: 'scroll-to-start',
-          title: 'Scroll to start',
-          isHidden: true,
-          hide: [0, 2, 4],
-          show: [1, 3, 5]
-        }, {
-          id: 'scroll-to-end',
-          title: 'Scroll to end',
-          isHidden: false,
-          hide: [1, 3, 5],
-          show: [0, 2, 4]
-        }, {
-          id: 'scroll-to-start-left',
-          title: 'Scroll to start left',
-          isHidden: true,
-          hide: [2],
-          show: [3]
-        }, {
-          id: 'scroll-to-end-left',
-          title: 'Scroll to end left',
-          isHidden: false,
-          hide: [3],
-          show: [2]
-        }, {
-          id: 'scroll-to-start-top',
-          title: 'Scroll to start top',
-          isHidden: true,
-          hide: [4],
-          show: [5]
-        }, {
-          id: 'scroll-to-end-top',
-          title: 'Scroll to end top',
-          isHidden: false,
-          hide: [5],
-          show: [4]
-        }]
-      })
+    this.state = {
+      'scroll-to-start': {
+        title: 'Scroll to start',
+        isHidden: true,
+        method: 'scrollToStart',
+        hide: ['scroll-to-start', 'scroll-to-start-left', 'scroll-to-start-top'],
+        show: ['scroll-to-end', 'scroll-to-end-left', 'scroll-to-end-top']
+      },
+      'scroll-to-end': {
+        title: 'Scroll to end',
+        isHidden: false,
+        method: 'scrollToEnd',
+        hide: ['scroll-to-end', 'scroll-to-end-left', 'scroll-to-end-top'],
+        show: ['scroll-to-start', 'scroll-to-start-left', 'scroll-to-start-top']
+      },
+      'scroll-to-start-left': {
+        title: 'Scroll to start left',
+        isHidden: true,
+        method: 'scrollToStartLeft',
+        hide: ['scroll-to-start-left'],
+        show: ['scroll-to-end-left']
+      },
+      'scroll-to-end-left': {
+        title: 'Scroll to end left',
+        isHidden: false,
+        method: 'scrollToEndLeft',
+        hide: ['scroll-to-end-left'],
+        show: ['scroll-to-start-left']
+      },
+      'scroll-to-start-top': {
+        title: 'Scroll to start top',
+        isHidden: true,
+        method: 'scrollToStartTop',
+        hide: ['scroll-to-start-top'],
+        show: ['scroll-to-end-top']
+      },
+      'scroll-to-end-top': {
+        title: 'Scroll to end top',
+        isHidden: false,
+        method: 'scrollToEndTop',
+        hide: ['scroll-to-end-top'],
+        show: ['scroll-to-start-top']
+      }
     }
   }
 
@@ -91,77 +90,69 @@ export default class ExposedMethodsEg1 extends React.Component {
     })
 
     this.nsa.init()
+
+    setTimeout(() => {
+      this.nsa.scrollToEnd()
+    }, 1000);
   }
 
   componentWillUnmount() {
     this.nsa.destroy()
   }
 
-  onActionClick(index) {
+  onActionClick(id) {
     if (!this.nsa) {
       return;
     }
 
-    const clickedItem = this.state.actionData[index]
+    const clickedItem = this.state[id]
 
     if (!clickedItem) {
       return;
     }
 
     const {
-      id,
+      method,
       show,
       hide
     } = clickedItem
-    let method;
-
-    switch (id) {
-      case 'scroll-to-start':
-        method = 'scrollToStart'
-        break;
-
-      case 'scroll-to-end':
-        method = 'scrollToEnd'
-        break;
-
-      case 'scroll-to-start-left':
-        method = 'scrollToStartLeft'
-        break;
-
-      case 'scroll-to-end-left':
-        method = 'scrollToEndLeft'
-        break;
-
-      case 'scroll-to-start-top':
-        method = 'scrollToStartTop'
-        break;
-
-      case 'scroll-to-end-top':
-        method = 'scrollToEndTop'
-        break;
-    
-      default:
-        break;
-    }
 
     this.nsa[method] && this.nsa[method]()
 
-    const actionData = [...this.state.actionData]
+    const withDivider = [...show, '||', ...hide]
+    let isHidden = false
+    let thatState = {}
 
-    show.forEach(index => actionData[index].isHidden = false)
-    hide.forEach(index => actionData[index].isHidden = true)
-    this.setState({ actionData })
+    withDivider.forEach(thatId => {
+      if (thatId === '||') {
+        isHidden = true;
+        return;
+      }
+
+      thatState = this.state[thatId]
+      this.setState({ [thatId]: Object.assign({}, thatState, { isHidden }) })
+    })
   }
 
   generateAction() {
+    const keys = Object.keys(this.state)
+
     return (
       <Actions>
         {
-          this.state.actionData.map(({ id, title, isHidden }, index) => (
-            <Actions.Item key={id} isHidden={isHidden} onClick={() => this.onActionClick(index)}>
-              <Actions.Button>{title}</Actions.Button>
-            </Actions.Item>
-          ))
+          keys.map((id) => {
+            const data = this.state[id];
+            const {
+              isHidden,
+              title
+            } = data;
+
+            return (
+              <Actions.Item key={id} isHidden={isHidden} onClick={() => this.onActionClick(id)}>
+                <Actions.Button>{title}</Actions.Button>
+              </Actions.Item>
+            )
+          })
         }
       </Actions>
     )
