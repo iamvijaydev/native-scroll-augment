1305e6b10c965082ccd1e2e80abc1a3567f08d79
Vijay Dev <vijaydev.in@gmail.com>
2018-02-06 20:22:40 +0530

    done with demos

--- src/components/exposedMethods/Actions.js
+++ src/components/exposedMethods/Actions.js
@@ -27,7 +27,7 @@ const Item = styled.div`
   position: relative;
   margin: 0 1rem;
   animation: ${show} .5s ease forwards;
-  flex-basis: ${props => props.isInput ? '8rem' : 'initial'};
+  flex-basis: ${props => props.isInput ? '7rem' : 'initial'};
 `
 
 const Button = styled.button`
@@ -45,6 +45,7 @@ const Button = styled.button`
   outline: none;
   overflow: hidden;
   text-overflow: ellipsis;
+  transition: all 0.3s ease;
 
   &:active {
     outline-color: #ff8e77;
@@ -52,26 +53,33 @@ const Button = styled.button`
     color: #fff7f3;
     background-color: #d84627;
   }
+
+  &:disabled {
+    background: #b1b1b1;
+    color: #6d6d6d;
+    border-color: #c1c1c1;
+    cursor: not-allowed;
+  }
 `
 
 const Input = styled.input`
   padding: 0.5rem 1rem;
   width: 100%;
   box-sizing: border-box;
-  border-radius: 35px;
+  border-radius: 3px;
   color: #037;
   font-size: 1.2rem;
   cursor: pointer;
-  border: 1px solid #e0edff;
+  border: 1px solid #396db3;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif;
   outline: none;
   overflow: hidden;
   text-overflow: ellipsis;
+  background: #ddecff;
 
-  &:active {
-    outline-color: #ff8e77;
-    box-shadow: 0 0 0 #af3b23;
-    color: #fff7f3;
+  &:focus {
+    border-color: #000;
+    background: #fff;
   }
 `
 
