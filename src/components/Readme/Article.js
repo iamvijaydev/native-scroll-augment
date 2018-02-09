47d5a7e8f862df43eefdf79edf1aaff69c4a77b3
Vijay Dev <vijaydev.in@gmail.com>
2018-02-09 11:09:23 +0530

    added main libaray updates

--- src/components/Readme/Article.js
+++ src/components/Readme/Article.js
@@ -7,33 +7,33 @@ const Article = styled.article`
   height: 100%;
   box-sizing: border-box;
 
-  h1 {
+  > h1 {
     font-size: 3rem;
     padding: 0 0 1rem 0;
     margin: 0 0 1rem 0;
     border-bottom: 1px solid #eaecef;
   }
 
-  h2 {
+  > h2 {
     font-size: 2rem;
     padding: 0 0 1rem 0;
     margin: 3rem 0 1rem 0;
     border-bottom: 1px solid #eaecef;
   }
 
-  h3 {
+  > h3 {
     font-size: 1.7rem;
     font-weight: 480;
     margin: 2.5rem 0 0 0;
   }
 
-  p {
+  > p {
     font-size: 1.5rem;
     margin: 2rem 0 2rem 0;
+  }
 
-    &:empty {
-      display: none;
-    }
+  p:empty {
+    display: none;
   }
 
   pre {
